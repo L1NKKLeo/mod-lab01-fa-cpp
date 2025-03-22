b618b21a0bca93bff63e765fa459af0048cd003d
L1NKKLeo <lenya.maltsev.95@mail.ru>
2025-03-23 01:06:02 +0300

    Мальцев Леонид 3822Б1ПИпр2

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,94 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str) {
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigits = false;
+
+    while (*str) {
+        if (*str != ' ') {
+            if (!inWord) {
+                inWord = true;
+                hasDigits = false;
+            }
+            if (*str >= '0' && *str <= '9') {
+                hasDigits = true;
+            }
+        }
+        else if (inWord) {
+            if (!hasDigits) {
+                count++;
+            }
+            inWord = false;
+        }
+        str++;
+    }
+    if (inWord && !hasDigits) {
+        count++;
+    }
+    return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+    unsigned int count = 0;
+    bool inWord = false;
+    bool validWord = true;
+    bool firstChar = true;
+
+    while (*str) {
+        if (*str != ' ') {
+            if (!inWord) {
+                inWord = true;
+                validWord = (*str >= 'A' && *str <= 'Z');
+                firstChar = true;
+            }
+            if (firstChar) {
+                firstChar = false;
+            }
+            else if (!(*str >= 'a' && *str <= 'z')) {
+                validWord = false;
+            }
+        }
+        else if (inWord) {
+            if (validWord) {
+                count++;
+            }
+            inWord = false;
+        }
+        str++;
+    }
+    if (inWord && validWord) {
+        count++;
+    }
+    return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) {
+    unsigned int wordCount = 0;
+    unsigned int totalLength = 0;
+    unsigned int currentLength = 0;
+    bool inWord = false;
+
+    while (*str) {
+        if (*str != ' ') {
+            if (!inWord) {
+                inWord = true;
+                currentLength = 0;
+            }
+            currentLength++;
+        }
+        else if (inWord) {
+            wordCount++;
+            totalLength += currentLength;
+            inWord = false;
+        }
+        str++;
+    }
+    if (inWord) {
+        wordCount++;
+        totalLength += currentLength;
+    }
+
+    if (wordCount == 0) return 0;
+    return (totalLength + wordCount / 2) / wordCount; // Округление
 }
