588f188dd103c6c6c9ed8bbad8464a779dc76cf6
L1NKKLeo <lenya.maltsev.95@mail.ru>
2025-03-23 11:21:47 +0300

    Update main.cpp try 2

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,13 @@ int main() {
     std::cout << "Test 2: " << faStr1(test2) << std::endl;
     std::cout << "Test 3: " << faStr1(test3) << std::endl;
 
-    std::cout << "\nTask 2 tests (words starting with capital, rest lowercase):" << std::endl;
+    std::cout << "Task 2 tests (words starting with capital, "
+        << "rest lowercase):" << std::endl;
     std::cout << "Test 1: " << faStr2(test1) << std::endl;
     std::cout << "Test 2: " << faStr2(test2) << std::endl;
     std::cout << "Test 3: " << faStr2(test3) << std::endl;
 
-    std::cout << "\nTask 3 tests (average word length):" << std::endl;
+    std::cout << "Task 3 tests (average word length):" << std::endl;
     std::cout << "Test 1: " << faStr3(test1) << std::endl;
     std::cout << "Test 2: " << faStr3(test2) << std::endl;
     std::cout << "Test 3: " << faStr3(test3) << std::endl;
