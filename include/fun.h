5f2c05c14e4c953fe42437dd20455fdaf2232af2
L1NKKLeo <lenya.maltsev.95@mail.ru>
2025-03-23 11:02:17 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -1,8 +1,9 @@
-#ifndef FUN_H
-#define FUN_H
+// Copyright 2022 UNN-IASR
+#ifndef INCLUDE_FUN_H_
+#define INCLUDE_FUN_H_
 
 unsigned int faStr1(const char* str);
 unsigned int faStr2(const char* str);
 unsigned int faStr3(const char* str);
 
-#endif
+#endif  // INCLUDE_FUN_H_
